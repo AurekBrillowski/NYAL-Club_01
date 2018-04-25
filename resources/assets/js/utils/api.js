fa9906e35ca18bfe12adc065e1c617563936dcf0
TheoBrixton <mediamail@lnx-technologies.com>
2018-04-25 14:06:43 -0500

    Structuring API Calls / HTTP Requests
    - Created "utils" directory that contains an api.js file.
    - The purpose of the api.js file is to hold the logic of  behind the HTTP requests to our REST API.
    - This makes it MUCH easier to follow and it is easily reusable for later use.

---
+++ resources/assets/js/utils/api.js
@@ -0,0 +1,11 @@
+export default {
+	organizations(endpoint) {
+		return {
+			getAll: () => axios.get(endpoint),
+			getOne: ({ id }) => axios.get(`${endpoint}/${id}`),
+			update: (toUpdate) =>  axios.put(endpoint,toUpdate),
+			create: (toCreate) =>  axios.post(endpoint,toCreate),
+			delete: ({ id }) =>  axios.delete(`${endpoint}/${id}`)
+		}
+	}
+}
