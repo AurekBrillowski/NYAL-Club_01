3b408a09ec3319fcd289404068830d66e999f44d
AurekBrillowski <aurek@parkcitiesxc.org>
2018-05-02 09:51:07 -0500

    API Update
    Initial API completed for the Event Names table.

--- app/Http/Controllers/EventNameController.php
+++ app/Http/Controllers/EventNameController.php
@@ -2,84 +2,101 @@
 
 namespace App\Http\Controllers;
 
-use App\Event_Name;
 use Illuminate\Http\Request;
+use App\EventName;
 
 class EventNameController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
+    /** ===============================================================
+     * 1a. RETRIEVE ALL: Display a listing of the resource.
+     * ================================================================
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         return EventName::all();
     }
+    // ----------------------------------------------------------------
 
-    /**
-     * Show the form for creating a new resource.
-     *
+
+    /** ===============================================================
+     * 1b. RETRIEVE ONE: Display the specified resource.
+     * ================================================================
+     * @param  \App\Event_eventName  $event_eventName
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function show($id)
     {
-        //
+        return EventName::findOrFail($id);
     }
+    // ----------------------------------------------------------------
 
-    /**
-     * Store a newly created resource in storage.
-     *
+
+    /** ===============================================================
+     * 2. DELETE: Remove the specified resource from storage.
+     * ================================================================
+     * @param  \App\Event_eventName  $event_eventName
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $eventName = EventName::findOrFail($id);
+
+        if($eventName->delete()) {
+            return $eventName;
+            // return new eventNameResource($eventName);
+        }
+    }
+    // ----------------------------------------------------------------
+
+
+    /** ===============================================================
+     * 3 & 4. CREATE: Store a newly created resource in storage.
+     * ================================================================
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Event_Name  $event_Name
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Event_Name $event_Name)
-    {
-        //
-    }
+        // $eventName = $request->isMethod('put') ? eventName::findOrFail($request->id) : new eventName;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Event_Name  $event_Name
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Event_Name $event_Name)
-    {
-        //
-    }
+        // PUT: Update
+        // POST: Create
+        if ($request->isMethod('put')) {
+            $eventName = EventName::findOrFail($request->id);
+        } else {
+            $eventName = new EventName;
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Event_Name  $event_Name
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Event_Name $event_Name)
-    {
-        //
-    }
+        $eventName->id = $request->input('id');
+        $eventName->event_name = $request->input('event_name');
+        $eventName->alpha_sort_name = $request->input('alpha_sort_name');
+        $eventName->NYAL_Season_id = $request->input('NYAL_Season_id');
+        $eventName->club_race = $request->input('club_race');
+        $eventName->school_race = $request->input('school_race');
+        $eventName->month_id = $request->input('month_id');
+        $eventName->race_website = $request->input('race_website');
+        $eventName->tentative = $request->input('tentative');
+        $eventName->confirmed = $request->input('confirmed');
+        $eventName->data_updated = $request->input('data_updated');
+        $eventName->current_year_date = $request->input('current_year_date');
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Event_Name  $event_Name
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Event_Name $event_Name)
-    {
-        //
+        if($eventName->save()) {
+            // return new eventNameResource($eventName);
+            return $eventName;
+            // return ($article);
+        }
     }
+    // ----------------------------------------------------------------
+
+
+
+
+
+
+    // #######################################################
+    // #######################################################
+
+
 }
